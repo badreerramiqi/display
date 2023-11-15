359452ec1f68b63d8311f60ae4a6d9d6cfb37c64
Badre ER-Ramiqi <147147111+badreerramiqi@users.noreply.github.com>
2023-11-15 17:49:03 +0100

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,9 @@
+mp=input("Entrer mot de passe : ")
+for i in range(3):
+    if mp=="DEV@2023":
+      print("Hello")
+      break
+    else :
+     fp = input("Entrer votre film préféré :")
+    
+    
